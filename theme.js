93832e802c945d154a29f689bbe0ad803a722618
Michael Neeley <micneeley14@gmail.com>
2022-06-14 14:48:10 -0400

    bigger wordmark

--- theme.js
+++ theme.js
@@ -1,5 +1,5 @@
 import ApollosConfig from '@apollosproject/config';
-import Svg, { Path } from 'react-native-svg';
+import Svg, { Path, Rect } from 'react-native-svg';
 import FRAGMENTS from '@apollosproject/ui-fragments';
 import { makeIcon } from '@apollosproject/ui-kit';
 
@@ -40,18 +40,19 @@ const ICONS = {
       fill="none"
       xmlns="http://www.w3.org/2000/Svg"
     >
+      <Rect x="8" y="10" width="25" height="22" fill="white" />
       <Path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M0 19.997C0 31.0424 6.4298 39.9978 19.9994 39.9978H40V19.997C40 6.66229 31.0472 -6.10352e-05 19.9994 -6.10352e-05C8.9543 -6.10352e-05 0 8.95319 0 19.997ZM23.6488 11.859H11.6227L11.6304 12.2504V29.9849H17.8921V20.4726C17.8921 18.1642 19.1075 16.8292 21.1817 16.8292C22.2803 16.8292 23.2971 17.381 23.6875 18.1992C24.0392 18.8681 24.0812 19.2195 24.0812 21.0164V29.9849H30.3456V18.7107C30.3456 16.2824 29.9538 15.0705 28.7008 13.8143C27.4448 12.5268 25.7626 11.859 23.6488 11.859Z"
-        fill={'#6BAC43'}
+        fill="#6BAC43"
       />
     </Svg>
   )),
   BrandLogo: makeIcon(({ size = 32, fill } = {}) => (
     <Svg
-      width="204"
-      height="40"
+      width={(size / 40) * 204}
+      height={size}
       viewBox="0 0 204 40"
       fill="none"
       xmlns="http://www.w3.org/2000/Svg"
